6492f0a972834b3d5e0c3f067a7f1bdb7980ba13
zhangxx <zhangxx@gionee.com>
2017-07-04 13:50:07 +0800

    上传LOG模块
    Change-Id: I8b7d8c6a2c0fb0f042d90cb91f6af2bacc4f3d74

--- android/src/com/nova/game/AndroidLauncher.java
+++ android/src/com/nova/game/AndroidLauncher.java
@@ -6,12 +6,17 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.nova.net.netty.ChannelManager;
 
+import nova.common.game.mahjong.handler.GameLogger;
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new SceGame(), config);
+
+		//初始化Log模块
+		GameLogger.create(new Log());
 
 		// 测试与netty服务器通信
 		// ChannelManager.getInstance().connect();
