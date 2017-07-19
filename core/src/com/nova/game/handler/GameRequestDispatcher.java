eb53fff58095ced8fbb2e2a9c25b197f6434d2bd
zhangxx <zhangxx@gionee.com>
2017-07-19 17:55:33 +0800

    上传playerId
    Change-Id: I60f5d61c3b1bfa0fabd3ac98b29d7adc67329af9

--- core/src/com/nova/game/handler/GameRequestDispatcher.java
+++ core/src/com/nova/game/handler/GameRequestDispatcher.java
@@ -12,6 +12,7 @@ import nova.common.game.mahjong.util.MahjGameCommand;
 
 public class GameRequestDispatcher {
     private ChannelManager mChannel;
+    private static final int TEST_PLAYER_ID = 100;
 
     public GameRequestDispatcher() {
         mChannel = ChannelManager.getInstance();
@@ -19,12 +20,14 @@ public class GameRequestDispatcher {
 
     public void startGame() {
         JsonObject json = new JsonObject();
+        json.addProperty("player", TEST_PLAYER_ID);
         json.addProperty("com", MahjGameCommand.REQUEST_GAME_START);
         mChannel.sendMessage(GameCommand.GAME_TYPE_MAHJ, json.toString());
     }
 
     public void startGame(int room) {
         JsonObject json = new JsonObject();
+        json.addProperty("player", TEST_PLAYER_ID);
         json.addProperty("com", MahjGameCommand.REQUEST_ROOM_GAME_START);
         json.addProperty("room", String.valueOf(room));
         mChannel.sendMessage(GameCommand.GAME_TYPE_MAHJ, json.toString());
