6fd4838f582b70df8a20d3d0a72c4dc5c2202c12
zhangxx <zhangxx@gionee.com>
2017-12-04 20:27:24 +0800

    游戏信息写文件
    Change-Id: Ia5cc4173b8b08a861e18ff95d184d1f9ccfb04fe

--- core/src/com/nova/game/model/LocalMahjController.java
+++ core/src/com/nova/game/model/LocalMahjController.java
@@ -4,6 +4,7 @@ import com.nova.game.handler.LocalMahjGameHandler;
 
 import nova.common.GameCommand;
 import nova.common.game.mahjong.MahjGameManager;
+import nova.common.game.mahjong.handler.FileLogRecorderManager;
 import nova.common.game.mahjong.handler.MahjGameDispatcher;
 import nova.common.game.mahjong.util.TestMahjConstant;
 import nova.common.room.RoomController;
@@ -12,13 +13,22 @@ import nova.common.room.RoomManager;
 public class LocalMahjController extends MahjController {
 
 	private MahjGameDispatcher mDispatcher;
+
+	public LocalMahjController() {
+		// 初始化LOG文件系统
+		FileLogRecorderManager.getInstance().setFilePath("/sdcard/sce/mahj/temp/");
+		FileLogRecorderManager.getInstance().startRecord();
+	}
 	
 	@Override
 	public void startGame(int roomId) {
 		super.startGame(roomId);
 		/**---测试程序开始---**/
-//		TestMahjConstant.setDebug(4);
+		// TestMahjConstant.setDebug(4);
 		/**---测试程序结束---**/
+		/**----开启测试LOG开始----**/
+		// MahjGameManager.debug = true;
+		/**----开启测试LOG结束----**/
 		RoomManager room = RoomController.getInstance(GameCommand.MAHJ_TYPE_GAME).getRoomManager(-1);
 		room.setRoomHandler(new LocalMahjGameHandler());
 		MahjGameManager gameManager = (MahjGameManager)room.getGameManager();
